fdbc6642f3c589165be4139e93b6102ce8b20bb8
NagarajDoddamani <doddamaninagaraj5@gmail.com>
2025-12-05 09:19:40 +0530

    the route for face id signup

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,12 +3,15 @@ import { Routes, Route } from "react-router-dom";
 
 import LoginPage from "./Pages/LoginHome.jsx";
 import SignUp from "./Pages/SignUp.jsx";
+import SignWithFace from "./Pages/SignWithFace.jsx";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/user/sign" element={<SignUp />} />
+      <Route path="/user/sign/face" element={<SignWithFace />} />
+      
     </Routes>
   );
 }
