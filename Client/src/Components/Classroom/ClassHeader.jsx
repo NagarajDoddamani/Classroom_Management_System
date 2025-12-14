6e7905ba9777a201eaed902236b8977932d99e2e
NagarajDoddamani <doddamaninagaraj5@gmail.com>
2025-12-14 11:01:20 +0530

    The updated components

--- Client/src/Components/Classroom/ClassHeader.jsx
+++ Client/src/Components/Classroom/ClassHeader.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-export default function ClassHeader({ subjectName, teacherName }) {
+export default function ClassHeader({ subjectName, teacherName, collegeName, subjectCode }) {
   return (
     <div className="bg-[#dfffad] rounded-b-3xl text-center py-6 shadow-md">
       <h1 className="text-3xl font-bold">{subjectName}</h1>
       <p className="text-lg mt-1">~ {teacherName}</p>
+      <p className="text-sm mt-1">{collegeName} | {subjectCode}</p>
     </div>
   );
 }
